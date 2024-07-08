07ccb091c699a8ba7e0c1991f1281ec0e30f1a87
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-08 18:24:41 +0500

    added some changes

--- src/app/(frontend)/_components/facilities/Facilities.jsx
+++ src/app/(frontend)/_components/facilities/Facilities.jsx
@@ -10,41 +10,49 @@ import Link from "next/link";
 
 const Facilities = () => {
   return (
-    <div className="flex">
-      <div className="w-1/2 p-16">
-        <p className="text-5xl font-semibold ">We have everything you need</p>
+    <div className="flex lg:flex-row flex-col">
+      <div className="lg:w-1/2 md:p-16 px-3 py-16 ">
+        <p className="md:text-4xl text-2xl font-semibold ">
+          We have everything you need
+        </p>
         <p className="mt-6 text-lg">
           Posuere morbi leo urna molestie at elementum eu facilisis sed. Diam
           phasellus vestibulum lorem sed risus ultricies tristique
         </p>
-        <div className="flex mt-6">
-          <p className="flex items-center gap-3 w-1/2 text-base">
-            <Wifi strokeWidth={1} className="w-16 h-16 text-primary" />
-            Free available high speed WiFi
-          </p>
-          <p className="flex items-center gap-3 w-1/2 text-base">
-            <MapPin strokeWidth={1} className="w-16 h-16 text-primary" />
-            Сonvenient location in the center
-          </p>
+        <div className="grid md:grid-cols-1">
+          <div className="flex md:mt-6 md:flex-row flex-col justify-center items-center">
+            <p className="flex md:flex-row flex-col items-center gap-3 md:mt-0 mt-4 md:w-1/2 text-base">
+              <Wifi strokeWidth={1} className="w-16 h-16 text-primary" />
+              Free available high speed WiFi
+            </p>
+            <p className="flex md:flex-row flex-col items-center gap-3 md:mt-0 mt-4 md:w-1/2 text-base">
+              <MapPin strokeWidth={1} className="w-16 h-16 text-primary" />
+              Сonvenient location in the center
+            </p>
+          </div>
+          <div className="flex md:mt-6 md:flex-row flex-col justify-center items-center">
+            <p className="flex md:flex-row flex-col items-center gap-3 md:mt-0 mt-4 md:w-1/2 text-base">
+              <Luggage strokeWidth={1} className="w-14 h-14 text-primary" />
+              Free storage of luggage of any size
+            </p>
+            <p className="flex md:flex-row flex-col items-center gap-3 md:mt-0 mt-4 md:w-1/2 text-base">
+              <CircleParking
+                strokeWidth={1}
+                className="w-14 h-14 text-primary"
+              />
+              Parking place allocated to you
+            </p>
+          </div>
         </div>
-        <div className="flex mt-6">
-          <p className="flex items-center gap-3 w-1/2 text-base">
-            <Luggage strokeWidth={1} className="w-14 h-14 text-primary" />
-            Free storage of luggage of any size
-          </p>
-          <p className="flex items-center gap-3 w-1/2 text-base">
-            <CircleParking strokeWidth={1} className="w-14 h-14 text-primary" />
-            Parking place allocated to you
-          </p>
-        </div>
-        <div className="mt-10 flex items-center gap-4">
-          <Button>Book now</Button>
+
+        <div className="mt-10 flex md:flex-row flex-col-reverse items-center gap-4">
+          <Button className="sm:w-auto w-full">Book now</Button>
           <Link href={"#"} className="flex gap-2 text-blue-900 font-semibold">
             More about <ArrowRight className="w-4 self-center" />
           </Link>
         </div>
       </div>
-      <div className="w-1/2 relative aspect-[11/10] overflow-hidden">
+      <div className="lg:w-1/2 relative lg:aspect-[11/10] overflow-hidden lg:mx-0 mx-3">
         <Image
           src={Facilitiec}
           width={800}
