dcaf0b01a1cdaeabb5c84fb983981a1b4e9b73f9
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-09 16:00:13 +0500

    added some changes

--- src/app/(frontend)/_components/rooms/_components/Choose.jsx
+++ src/app/(frontend)/_components/rooms/_components/Choose.jsx
@@ -3,12 +3,12 @@ import React from "react";
 
 const Choose = () => {
   return (
-    <div className=" lg:max-w-[360px] lg:mx-0  mt-9 shadow-custom p-11 bg-primary text- rounded-md">
+    <div className=" lg:max-w-[360px] lg:mx-0  mt-9 shadow-custom p-11 bg-primary text-white rounded-md">
       <p className="text-4xl font-bold ">Stay Longer, Save More</p>
       <p className="text-lg mt-4">
         It&apos;s simple the longer you stay, the more you save!
       </p>
-      <p className="border-l-2 border-primary pl-4 mt-4">
+      <p className="border-l-2 border-white pl-4 mt-4">
         Save up to 30% on daily rate for stays longer than 14 nights <br />
         <br />
         Save up to 20% off the nightly rate on stays between 7-14 nights
