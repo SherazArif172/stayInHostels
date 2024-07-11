388bf297f2e196688ef329a2d9534dec93e20908
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-11 18:16:00 +0500

    fixed map

--- src/app/(frontend)/_components/contacts/Contacts.jsx
+++ src/app/(frontend)/_components/contacts/Contacts.jsx
@@ -4,16 +4,18 @@ import React from "react";
 
 const Contacts = () => {
   return (
-    <div className="flex max-w-7xl m-auto py-16">
-      <div className="px-16 self-center">
-        <p className="text-4xl font-semibold">Contacts</p>
-        <p className="mt-5">
+    <div className="flex lg:flex-row flex-col max-w-7xl m-auto py-16 lg:gap-0 gap-8 lg:px-0 px-4">
+      <div className="lg:px-16 self-center w-full">
+        <p className="text-4xl font-semibold md:text-left text-center">
+          Contacts
+        </p>
+        <p className="mt-5 md:text-left text-center">
           Egestas pretium aenean pharetra magna ac. Et tortor consequat id porta
           nibh venenatis cras sed
         </p>
         <div className="mt-7 ">
-          <div className="flex gap-7">
-            <div className="flex justify-center items-center gap-5">
+          <div className="flex md:flex-row flex-col gap-7">
+            <div className="flex  justify-center items-center gap-5">
               <div className="p-7 bg-primary-cstm rounded-lg">
                 <Phone
                   className="self-center text-primary w-7 h-7"
@@ -54,7 +56,7 @@ const Contacts = () => {
               </div>
             </div>
           </div>
-          <div className="flex gap-7 mt-6">
+          <div className="flex md:flex-row flex-col gap-7 mt-6">
             <div className="flex justify-center items-center gap-5">
               <div className="p-7 bg-primary-cstm rounded-lg">
                 <Phone
@@ -87,12 +89,11 @@ const Contacts = () => {
           </div>
         </div>
       </div>
-      <div className=" ">
+
+      <div className="relative w-full h-[500px]  pb-56">
         <iframe
+          className="absolute inset-0 w-full h-full"
           src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d27010.6093473729!2d73.0263226!3d32.1954407!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3921947a5fb8d879%3A0xddcf7cf655df0169!2sTHQ%20Hospital%20Kot%20Momin!5e0!3m2!1sen!2s!4v1720701466414!5m2!1sen!2s"
-          width="500"
-          height="450"
-          // style="border:0;"
           allowfullscreen=""
           loading="lazy"
           referrerpolicy="no-referrer-when-downgrade"
