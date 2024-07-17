09af054680a2fa35be6a01c19c93422e00a5a3c5
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-17 15:55:42 +0500

    added a change

---
+++ src/app/(frontend)/about/_components/Faq/Faqs.jsx
@@ -0,0 +1,37 @@
+"use client";
+
+import React from "react";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
+import Faq from "../../../../../data/faq.json";
+
+const Faqs = () => {
+  return (
+    <div className="md:px-16 px-4 lg:py-16 py-8">
+      <div className="flex lg:flex-row flex-col justify-between lg:gap-40">
+        <p className="md:text-4xl text-3xl font-bold">
+          Frequently asked questions about hostel
+        </p>
+        <p className="lg:text-right lg:mt-0 mt-3">
+          Diam phasellus vestibulum lorem sed risus ultricies tristique
+        </p>
+      </div>
+      <div className="mt-8">
+        {Faq.map((faq, index) => (
+          <Accordion type="single" collapsible className="w-full">
+            <AccordionItem value="item-1" key={index}>
+              <AccordionTrigger>{faq.question}</AccordionTrigger>
+              <AccordionContent>{faq.answer}</AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+export default Faqs;
