79bd52849c650566d740dcd241c9c1e1894fca03
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-17 13:54:31 +0500

    added responsiveness

--- src/app/(frontend)/about/_components/Question/Question.jsx
+++ src/app/(frontend)/about/_components/Question/Question.jsx
@@ -6,46 +6,46 @@ import { Button } from "@/components/ui/button";
 
 const Question = () => {
   return (
-    <div className="px-16 py-16 flex gap-8">
+    <div className="lg:px-16 px-4 lg:py-16 py-8 flex lg:flex-row flex-col gap-8">
       <div className="w-full lg:self-center">
         <p className="md:text-4xl text-3xl font-bold">
           Rule settlement, eviction and stay
         </p>
         <div className="text-lg mt-7">
-          <p className="flex">
+          <p className="flex gap-2">
             <Check /> Time of arrival is after 14-00. Time of departure is to
             12-00
           </p>{" "}
           <br />
-          <p className="flex">
+          <p className="flex gap-2">
             <Check />
             Does a settlement take place only at complete payment
           </p>{" "}
           <br />
-          <p className="flex">
+          <p className="flex gap-2">
             <Check className="" /> Is there a settlement in hostel only after
             the presence of passport
           </p>
           <br />
-          <p className="flex">
-            <Check />
+          <p className="flex gap-2">
+            <Check className="w-20 " />
             Volutpat odio facilisis mauris sit amet massa vitae tortor
             condimentum. Quam elementum pulvinar etiam non quam lacus
             suspendisse. Eget gravida cum sociis natoque
           </p>
         </div>
       </div>
-      <div className="w-full bg-white shadow-custom p-16">
+      <div className="w-full bg-white shadow-custom md:px-16 px-4 md:py-16 py-8 rounded-lg max-w-full">
         <p className="md:text-4xl text-3xl font-bold">
           We are ready answer your question{" "}
         </p>
         <div className="mt-10">
-          <div className="flex gap-6">
-            <div className="border px-5 border-black rounded-lg flex justify-between items-center">
+          <div className="flex sm:flex-row flex-col gap-6">
+            <div className="border w-full px-5 border-black rounded-lg flex justify-between items-center">
               <Input type="email" placeholder="Email" />{" "}
               <User strokeWidth={1} />
             </div>
-            <div className="border px-5 border-black rounded-lg flex justify-between items-center">
+            <div className="border w-full px-5 border-black rounded-lg flex justify-between items-center">
               <Input type="email" placeholder="Email" />{" "}
               <MailOpen strokeWidth={1} />
             </div>
@@ -54,7 +54,7 @@ const Question = () => {
             <Textarea placeholder="Message" />
           </div>
         </div>
-        <Button className="mt-10">Send message</Button>
+        <Button className="mt-10 sm:w-auto w-full">Send message</Button>
       </div>
     </div>
   );
