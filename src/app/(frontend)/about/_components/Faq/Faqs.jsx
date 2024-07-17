0447ac9f4374564d0de4af22bea69008ea298d4d
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-17 14:06:22 +0500

    added data

--- src/app/(frontend)/about/_components/Faq/Faqs.jsx
+++ src/app/(frontend)/about/_components/Faq/Faqs.jsx
@@ -7,18 +7,19 @@ import {
   AccordionItem,
   AccordionTrigger,
 } from "@/components/ui/accordion";
+import Faq from "../../../../../data/faq.json";
 
 const Faqs = () => {
   return (
     <div className="md:px-16 px-4 lg:py-16 py-8">
-      <Accordion type="single" collapsible className="w-full">
-        <AccordionItem value="item-1">
-          <AccordionTrigger>Is it accessible?</AccordionTrigger>
-          <AccordionContent>
-            Yes. It adheres to the WAI-ARIA design pattern.
-          </AccordionContent>
-        </AccordionItem>
-      </Accordion>
+      {Faq.map((faq, index) => (
+        <Accordion type="single" collapsible className="w-full">
+          <AccordionItem value="item-1" key={index}>
+            <AccordionTrigger>{faq.question}</AccordionTrigger>
+            <AccordionContent>{faq.answer}</AccordionContent>
+          </AccordionItem>
+        </Accordion>
+      ))}
     </div>
   );
 };
