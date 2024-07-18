f18d214c7fa91136c9dbaafd7d2aedbbdb354249
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-18 23:55:13 +0500

    added owners

--- src/app/(frontend)/about/_components/Owner/Owner.jsx
+++ src/app/(frontend)/about/_components/Owner/Owner.jsx
@@ -8,24 +8,23 @@ import { ArrowRight } from "lucide-react";
 const Owner = () => {
   return (
     <div className="lg:py-16 py-8 lg:px-16 px-4 max-w-7xl m-auto">
-      <div className="flex items-center gap-10">
-        <div className="relative w-44 aspect-square rounded-full">
+      <div className="flex md:flex-row flex-col md:items-center gap-10">
+        <div className="relative md:w-44 w-40 aspect-square rounded-full">
           <Image src={Ownerimg} fill className="object-cover rounded-full" />
         </div>
-        <div className="w-4/6">
+        <div className="md:w-4/6">
           <p className="font-semibold text-xl">
-            Mauris a diam maecenas sed enim ut sem. Scelerisque in dictum <br />{" "}
-            non consectetur a erat nam. Commodo viverra maecenas <br /> accumsan
-            lacus
+            Mauris a diam maecenas sed enim ut sem. Scelerisque in dictum non
+            consectetur a erat nam. Commodo viverra maecenas accumsan lacus
           </p>
-          <div className="flex mt-2 gap-11">
+          <div className="flex md:flex-row flex-col mt-2 md:gap-11 ">
             <p>Johnathan Jennings </p>
             <p>Founder of the hostel Hosteller</p>
           </div>
         </div>
       </div>
-      <div className="mt-10 flex gap-16">
-        <div className="pl-8 border-l-2 border-primary w-1/2">
+      <div className="mt-10 flex md:flex-row flex-col md:gap-16 gap-8">
+        <div className="md:pl-8 pl-4 border-l-2 border-primary md:w-1/2">
           <p className="font-semibold text-xl ">
             Are you looking for an apartment?
           </p>
@@ -41,9 +40,9 @@ const Owner = () => {
             Urna id volutpat lacus laoreet. Viverra vitae <br /> congue eu
             consequat ac
           </p>
-          <div className="border border-black mt-10 w-80 rounded-lg px-4 flex items-center">
+          <div className="border border-black md:mt-10 mt-7 w-80 rounded-lg px-4 flex items-center ">
             <Input placeholder="Email adress" />{" "}
-            <div className="animate-pulse relative z-10">
+            <div className="animate-pulse relative z-10 h-full">
               <ArrowRight className="w-8  " />
             </div>
           </div>
