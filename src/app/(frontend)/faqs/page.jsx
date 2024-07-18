d058dc5c66bbdcbb9f3d8a1996bec5fd08d23b5c
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-19 02:16:50 +0500

    resolved issue

--- src/app/(frontend)/faqs/page.jsx
+++ src/app/(frontend)/faqs/page.jsx
@@ -12,17 +12,19 @@ import {
 
 const page = () => {
   return (
-    <div className="bg-primary-cstm h-44 p-14 max-w-7xl m-auto">
-      <div className="text-lg">
+    <div className=" max-w-7xl m-auto">
+      <div className="text-lg h-44 bg-primary-cstm p-14">
         <Link href="/" className="text-primary font-semibold">
           Home
         </Link>{" "}
         / Frequently asked questions
+        <p className="text-5xl font-semibold mt-2">
+          Frequently asked questions
+        </p>
       </div>
-      <p className="text-5xl font-semibold mt-2">Frequently asked questions</p>
 
-      <div className="mt-8">
-        <Accordion type="single" collapsible className="w-full">
+      <div className="lg:py-16 py-8 lg:px-16 px-4">
+        <Accordion type="single" collapsible className="w-full h-full">
           {Faq.map((faq, index) => (
             <AccordionItem value="item-1" key={index}>
               <AccordionTrigger>{faq.question}</AccordionTrigger>
