58daff0d3aaa17ca96a1902e4df9fd970d8feaca
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-19 02:05:59 +0500

    added faq

--- src/app/(frontend)/faqs/page.jsx
+++ src/app/(frontend)/faqs/page.jsx
@@ -1,10 +1,36 @@
+"use client";
+
 import React from "react";
-import Faqs from "../about/_components/Faq/Faqs";
+import Link from "next/link";
+import Faq from "../../../data/faq.json";
+import {
+  Accordion,
+  AccordionContent,
+  AccordionItem,
+  AccordionTrigger,
+} from "@/components/ui/accordion";
 
 const page = () => {
   return (
-    <div>
-      <Faqs />
+    <div className="bg-primary-cstm h-44 p-14 max-w-7xl m-auto">
+      <div className="text-lg">
+        <Link href="/" className="text-primary font-semibold">
+          Home
+        </Link>{" "}
+        / Frequently asked questions
+      </div>
+      <p className="text-5xl font-semibold mt-2">Frequently asked questions</p>
+
+      <div className="mt-8">
+        <Accordion type="single" collapsible className="w-full">
+          {Faq.map((faq, index) => (
+            <AccordionItem value="item-1" key={index}>
+              <AccordionTrigger>{faq.question}</AccordionTrigger>
+              <AccordionContent>{faq.answer}</AccordionContent>
+            </AccordionItem>
+          ))}
+        </Accordion>
+      </div>
     </div>
   );
 };
