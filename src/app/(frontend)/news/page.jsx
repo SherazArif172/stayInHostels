844fb573bd9ca09ce3fe4c4228a0ee1aa00680fb
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-28 12:04:47 +0500

    making news page

--- src/app/(frontend)/news/page.jsx
+++ src/app/(frontend)/news/page.jsx
@@ -1,11 +1,17 @@
+import Link from "next/link";
 import React from "react";
 
 const news = () => {
   return (
-    <div className="lg:py-16 py-8 lg:px-16 px-4 ">
-      <p className="py-8 text-center text-3xl font-semibold">
-        This Page is Under-Development
-      </p>
+    <div className=" ">
+      <div className="text-lg h-44 bg-primary-cstm md:px-14 px-11 md:py-14 py-10 max-w-7xl m-auto">
+        <Link href="/" className="text-primary font-semibold">
+          Home
+        </Link>{" "}
+        / News
+        <p className="md:text-5xl text-2xl font-semibold mt-2">News</p>
+      </div>
+      <div className="max-w-7xl m-auto py-16 "></div>
     </div>
   );
 };
