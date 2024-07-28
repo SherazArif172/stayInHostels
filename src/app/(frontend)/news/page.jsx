7f8cef60182c1088d6f8578a61fe947d3a30de43
Sheraz Arif <sherazarifofficial@gmail.com>
2024-07-28 12:35:33 +0500

    addimg news pagw half has been done

--- src/app/(frontend)/news/page.jsx
+++ src/app/(frontend)/news/page.jsx
@@ -1,5 +1,10 @@
-import Link from "next/link";
 import React from "react";
+import { Button } from "@/components/ui/button";
+import Blogss from "../../../data/blogs.json";
+import Image from "next/image";
+import { CalendarDays, Eye } from "lucide-react";
+import Link from "next/link";
+import { motion } from "framer-motion";
 
 const news = () => {
   return (
@@ -11,7 +16,59 @@ const news = () => {
         / News
         <p className="md:text-5xl text-2xl font-semibold mt-2">News</p>
       </div>
-      <div className="max-w-7xl m-auto py-16 "></div>
+      <div className="lg:px-16 px-4 lg:py-16 py-8 max-w-7xl m-auto">
+        <div className="mt-8  flex">
+          <div className="">
+            {" "}
+            {Blogss.map((blogs, index) => (
+              <div
+                // initial={{ y: 150, opacity: 0.5 }}
+                // whileInView={{ y: 0, opacity: 1 }}
+                // viewport={{ once: true }}
+                // transition={{ duration: 0.5 }}
+                className="shadow-custom flex justify-center items-center mt-7 group"
+                key={index}
+              >
+                <div className="relative  aspect-[5/4] w-1/2 overflow-hidden rounded-l-lg">
+                  <Image
+                    src={blogs.image}
+                    fill
+                    className="w-full h-full object-cover group-hover:scale-110 duration-500 rounded-l-lg"
+                    alt="image"
+                  />
+                  <p className="absolute right-0 top-7 bg-white p-2 rounded-l-lg text-primary font-semibold">
+                    {blogs.topic}
+                  </p>
+                </div>
+                <div className="p-8">
+                  <Link
+                    href="#"
+                    className="text-xl font-semibold hover:text-primary duration-300"
+                  >
+                    {blogs.title}
+                  </Link>
+                  <p className="mt-3 text-lg">{blogs.description}</p>
+                  <div className="mt-5 flex justify-between">
+                    <p className="flex gap-1 justify-center items-center">
+                      <CalendarDays
+                        className="text-primary w-6 h-6"
+                        strokeWidth={1}
+                      />
+                      {blogs.date}
+                    </p>
+                    <p className="flex gap-1 justify-center items-center">
+                      <Eye className="text-primary w-6 h-6" strokeWidth={1} />
+                      {blogs.views}
+                    </p>
+                    <div></div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+          <div>dkj</div>
+        </div>
+      </div>{" "}
     </div>
   );
 };
