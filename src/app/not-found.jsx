46951418b64e84cc75c746ca1da94315ced9ab26
Tayyab Junaid <tayyabjunaidofficial@gmail.com>
2024-08-07 21:45:17 +0500

    merged main

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,7 +1,21 @@
 import React from "react";
 
 const NotFound = () => {
-  return <div>not fund</div>;
+  return (
+    <div>
+      <div>
+        <p>404</p>
+        <p>This is not the page you are looking for!</p>
+        <p>
+          Tellus in metus vulputate eu scelerisque felis imperdiet proin. Sed
+          faucibus turpis in eu mi bibendum
+        </p>
+      </div>
+      <div>
+        <Image src={not_found} />
+      </div>
+    </div>
+  );
 };
 
 export default NotFound;
