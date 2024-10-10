5150c502cb4b3bc4e59cf882f4da1475bfee166b
Sheraz Arif <sherazarifofficial@gmail.com>
2024-10-11 01:35:36 +0500

    added favicon.ico

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -3,9 +3,10 @@ import { Mulish } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/navbar/Navbar";
 import Footer from "@/components/footer/footer";
+import Head from "next/head"; // Import the Head component from Next.js
 
 export const metadata = {
-  title: "stay inn hostels",
+  title: "stayinn hostels",
   description:
     "Discover affordable and comfortable hostel accommodations at Stay Inn Hostels. Whether you're traveling for business, studying, or exploring new cities, our modern and convenient hostels offer the perfect place to stay. Enjoy amenities like free Wi-Fi, cozy rooms, and easy access to local attractions. Book your stay today!",
 };
@@ -18,6 +19,9 @@ const mulish = Mulish({
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <Head>
+        <link rel="icon" href="/favicon.ico" type="image/x-icon" />
+      </Head>
       <body className={mulish.className}>
         <Navbar />
         {children}
